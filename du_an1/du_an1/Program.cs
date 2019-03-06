bc546d53c82fa0fba2399bdb235e153d7bb0b9b8
Trần Sắc <sactd.kd01@gmail.com>
2019-03-06 17:32:07 +0700

    du an
    thay doi

---
+++ du_an1/du_an1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace du_an1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contact> danhba = new List<Contact>();
+            char r;
+            do
+            {
+                Console.WriteLine("Moi nhap thong tin:");
+                Console.Write("Ho:");
+                string ho = Console.ReadLine();
+                Console.Write("Ten:");
+                string ten = Console.ReadLine();
+                Console.Write("Dia chi:");
+                string diachi = Console.ReadLine();
+                Console.Write("So dien thoai:");
+                string sodt = Console.ReadLine();
+                Contact co = new Contact(ho, ten, diachi, sodt);
+                danhba.Add(co);
+                Console.WriteLine("Ban co muon tiep tuc khong:(y/n)");
+                r = Console.ReadKey().KeyChar;
+            }
+            while (r == 'y');
+            Console.WriteLine("/n Danh sach danh ba gom:");
+            foreach (Contact c in danhba)
+            {
+                Console.WriteLine("{0} - {1} - {2} - {3}", c.GetHo(), c.GetTen(), c.GetDiachi(), c.GetSodt());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ du_an1/du_an1/contact.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace du_an1
+{
+    class Contact
+    {
+        string ho;
+        string ten;
+        string diachi;
+        string sodt;
+        public Contact(string ho, string ten, string diachi, string sodt)
+        {
+            this.ho = ho;
+            this.ten =ten;
+            this.diachi = diachi;
+            this.sodt = sodt;
+        }
+        public void SetHo(string ho)
+        {
+            this.ho = ho;
+        }
+        public string GetHo()
+        {
+            return this.ho;
+        }
+        public void SetTen(string ten)
+        {
+            this.ten = ten;
+        }
+        public string GetTen()
+        {
+            return this.ten;
+        }
+        public void SetDiachi(string diachi)
+        {
+            this.diachi = diachi;
+        }
+        public string GetDiachi()
+        {
+            return this.diachi;
+        }
+        public void SetSodt(string sodt)
+        {
+            this.sodt = sodt;
+        }
+        public string GetSodt()
+        {
+            return this.sodt;
+        }
+    }
+}
